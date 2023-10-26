cab36f4dba47cd315f63f06358fd44cd19644ecd
mankibogames <138549051+mankibogames@users.noreply.github.com>
2023-10-26 13:41:44 +0700

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1578490.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 108929828, 108929829, 108929830, 108929832, 108929833, 108929834, 108929835, 108929842, 108929839, 108929846, 108929845, 108929847, 108929850, 108929851, 108929898, 109018289, 109189388, 109201881, 109215234 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/108929857/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/108929858/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/108929859/1/ammo.js', 'preload' : true},
+];
